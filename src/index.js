745350d5b150dbe6cf3ccf0764fa47c8d63b149d
Michael Gioeni <gioenijr@oswego.edu>
2023-02-09 11:04:29 -0700

    first commit only movie title and runtime are working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
-// Your code here
+const apiUrl = 'http://localhost:3000/films'
+
+const movieMenu = document.querySelector('#films');
+
+fetch(apiUrl)
+.then(response => response.json())
+.then((movieData) => movieData.forEach(renderMovie))
+
+function renderMovie(movies) {
+const movieli = document.createElement('li');
+movieli.innerText = movies.title;
+movieMenu.appendChild(movieli);
+
+movieli.addEventListener('click', (e) => movieClickHandler(movies))
+}
+function movieClickHandler(movies) {
+    el('title').textContent =movies.title;
+    el('runtime').textContent = movies.runtime + " " + "Minutes";
+    el('capacity').textContent = movies.capacity;
+    el('showtime').textContent = movies.showtime;
+    //el("tickets_sold").textContent= movie.tickets_sold;
+    el('film-info').textContent = movies.description;
+    el('posterImage').src = movies.poster;
+}
+function el(id){
+    return document.getElementById(id)
+}
+function movieHandler(e){
+    e.preventDefault();
+    
+    const newMovie = {
+    title: e.target.title.value,
+    runtime: e.target.runtime.value,
+    capacity: e.target.capacity.value,
+    showtime: e.target.showtime.value,
+    tickets_sold: e.target.tickets_sold.value,
+    description: e.target.description.value,
+    posterImage: e.target.poster.value,
+    }
+    
+}
+
+renderMovie(newMovie);
+
+
+
