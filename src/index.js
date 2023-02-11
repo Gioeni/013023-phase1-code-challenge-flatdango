f267714e1c086ea75b52413ebd86370aeb13d0ec
Michael Gioeni <gioenijr@oswego.edu>
2023-02-11 10:40:44 -0700

    fixed counter, fixed menu and fixed first movie info populating

--- src/index.js
+++ src/index.js
@@ -1,52 +1,92 @@
-const apiUrl = 'http://localhost:3000/films'
+const firstMovieAPI = 'http://localhost:3000/films/1'
+const movieAPI = 'http://localhost:3000/films'
+const MovieMenu = document.querySelector('#films')
 
-const movieMenu = document.querySelector('#films');
-const buyTicket = document.getElementById('buy-ticket');
-
-fetch(apiUrl)
+fetch(firstMovieAPI)
 .then(response => response.json())
-.then((movieData) => movieData.forEach(renderMovie))
+.then(data => renderFirstMovie(data))
 
-function renderMovie(movies) {
-const movieli = document.createElement('li');
-movieli.innerText = movies.title;
-movieMenu.appendChild(movieli);
+fetch(movieAPI)
+.then(response => response.json())
+.then(data => {
+    data.forEach((data) => {
+        renderMovies(data)
+    })
+    //console.log(data)
+});
 
-movieli.addEventListener('click', (e) => movieClickHandler(movies))
-}
-function movieClickHandler(movies) {
-    el('title').textContent =movies.title;
-    el('runtime').textContent = movies.runtime + " " + "Minutes";
-    //el('capacity').textContent = movies.capacity;
-    el('showtime').textContent = movies.showtime;
-    //el("tickets_sold").textContent= movie.tickets_sold;
-    el('film-info').textContent = movies.description;
-    el('poster').src = movies.poster;
-}
 function el(id){
     return document.getElementById(id)
 }
-function movieHandler(e){
-    e.preventDefault();
+function renderFirstMovie(data) {
+    const firstMoviePoster = document.getElementById("poster")
+    firstMoviePoster.src= data.poster
+    const firstMovieTitle = document.getElementById("title")
+    firstMovieTitle.textContent = data.title
+    const firstMovieRuntime = document.getElementById("runtime")
+    firstMovieRuntime.textContent = data.runtime + " " + "minutes"
+    const firstMovieinfo = document.getElementById("film-info")
+    firstMovieinfo.textContent = data.description
+    const firstMovieShowtime = document.getElementById("showtime")
+    firstMovieShowtime.textContent = data.showtime
     
-    const newMovie = {
-    title: e.target.title.value,
-    runtime: e.target.runtime.value,
-    capacity: e.target.capacity.value,
-    showtime: e.target.showtime.value,
-    tickets_sold: e.target.tickets_sold.value,
-    description: e.target.description.value,
-    posterImage: e.target.poster.value,
-    }
-
-//buyTicket.addEventListener('click', (event) => buyTicketHandler(newMovie))
     
-}    
-//buyTicketHandler(newMovie){
-   //el('ticket-num').innerTect = capacity - tickets-sold;
-//}
+    const ticketCount = document.getElementById("ticket-num")
+    ticketCount.innerHTML = parseInt(data.capacity) - parseInt(data.tickets_sold)
 
-//renderMovie(newMovie);
+    const button = document.getElementById("buy-ticket")
+    button.addEventListener('click', () => ticketsRemaining(data))
 
+
+}
+
+function renderMovies(data) {
+    const movieLi = document.createElement('li')
+    movieLi.innerText = data.title
+    MovieMenu.appendChild(movieLi); 
+
+    
+}
+function ticketsRemaining(data) {
+    const ticketCount = document.getElementById("ticket-num")
+    ticketCount.innerHTML = Math.max(0, data.capacity - (data.tickets_sold +=1))
+    // ticketCount.textContent = data.capacity - data.tickets_sold
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function movieClickHandler(apiData) {
+//     el('title').textContent= apiData.title
+//     el('poster').src= apiData.poster
+//     el('runtime').textContent= apiData.runtime + " " + "minutes"
+//     el('film-info').textContent= apiData.description
+//     el('showtime').textContent= apiData.showtime
+// }
+
+
+
+
+
+    
+
+//     movieMenu.src= data.poster
+//     const movieTitleMenu = document.getElementById("title")
+//     movieTitleMenu.textContent = data.title
+//     const firstMovieRuntime = document.getElementById("runtime")
+//     movieRuntimeMenu.textContent = data.runtime
+//     const movieinfoMenu = document.getElementById("film-info")
+//     movieinfoMenu.textContent = data.description
+//     const movieShowtimeMenu = document.getElementById("showtime")
+//     movieShowtimeMenu.textContent = data.showtime
 
 
